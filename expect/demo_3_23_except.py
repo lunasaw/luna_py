245d037f479dc9bcd917deacd2e91e8f6d406968
luna <iszychen@gmail.com>
2020-03-24 00:25:42 +0800

    traceback 模块

--- expect/demo_3_23_except.py
+++ expect/demo_3_23_except.py
@@ -8,3 +8,43 @@ try:
 	b = 1 / 0
 except BaseException:
 	print("这是一个异常噢!")
+
+# 多个异常 子类=>父类 依次捕捉
+try:
+	a = input("请输入一个被除数: ")
+	b = input("请输入一个除数: ")
+	c = float(a) / float(b)
+	print(c)
+except ZeroDivisionError:
+	print("异常, 除数不能为0")
+except ValueError:
+	print("不能将字符转为数字")
+except NameError:
+	print("变量不存在")
+except BaseException as e:
+	print(e)
+
+# _else 结构 try 如果异常执行异常 无异常执行else
+
+try:
+	a = input("请输入一个被除数: ")
+	b = input("请输入一个除数: ")
+	c = float(a) / float(b)
+except BaseException as e:
+	print(e)
+else:
+	print(c)
+	print("我是else 程序结束")
+
+# finally  无论是否发生异常都执行 关闭try中打开的操作
+
+try:
+	a = input("请输入一个被除数: ")
+	b = input("请输入一个除数: ")
+	c = float(a) / float(b)
+except BaseException as e:
+	print(e)
+else:
+	print(c)
+finally:
+	print("我是finally 程序结束")
---
+++ expect/demo_3_24_except_finally.py
@@ -0,0 +1,48 @@
+# -*- coding:utf-8 -*-
+# @Time: 2020/3/24 0:01
+# @Author: luna
+# @Email: 15696756582@163.com
+# @File: demo_3_24_except_finally.py
+
+try:
+	f = open("../io/a.txt", "r")
+	content = f.readline()
+	print(content)
+except:
+	print("文件未找到")
+finally:
+	print("我是finally 执行关闭资源")
+	try:
+		f.close()
+	except:
+		print("文件关闭失败")
+print("程序结束")
+
+# with 上下文管理器 文件被打开都会自行关闭
+# 序列化
+import pickle
+
+with open(r"../io/b.dat", "rb") as m:
+	b1 = pickle.load(m)
+	b2 = pickle.load(m)
+	b3 = pickle.load(m)
+	print(b1)
+	print(b2)
+	print(b3)
+
+# trackback 模块
+import traceback
+
+# try:
+# 	print("step1")
+# 	num = 1 / 0
+# except:
+# 	traceback.print_exc()  # 追溯异常信息
+
+'''将异常信息输出到文件'''
+try:
+	print("step1")
+	num = 1 / 0
+except:
+	with open("except.txt", "a") as f:
+		traceback.print_exc(file=f)  # 追溯异常信息输入到文件  不在控制台报错
