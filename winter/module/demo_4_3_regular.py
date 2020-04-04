84c1911eeab26645692d2a8421f63385264c58d8
luna <iszychen@gmail.com>
2020-04-04 23:29:21 +0800

    regular_2

--- winter/module/demo_4_3_regular.py
+++ winter/module/demo_4_3_regular.py
@@ -58,3 +58,46 @@ print(qq)
 
 qq = re.match('^[1-9][0-9]{9,}$', '11732822542131231231')  # {m,} 用于验证m次或者多次 >=m 次
 print(qq)
+
+# 验证用户名
+username = 'admin123'
+user = re.search('[a-zA-Z][0-9a-zA-Z]{5,}$', username)  # 数字不能打头 5位以上 整个匹配
+print(user)
+
+# 可以是字母或者数字_ 不能数字开头 长度6位以上
+user = re.search('^[a-zA-Z]\w{5,}$', username)  # \w 任意数字字母下划线组合
+print(user)
+
+msg = "aa.py ab.py eriewpy"
+# \b 可以匹配 pyhton文件的py  但是不能字符串的py
+msg = re.findall(r'\w*\.py\b', msg)  # r 原意输出 . 用于匹配除换行符之外的所有字符
+print(msg)
+
+'''
+	. 任意字符除(\n)
+	^ 开头
+	$ 结尾 match 整体匹配 返回一个 search 从头匹配 返回一个
+	[] 范围 [abc] [a-z] [A-Z] [**&#!@]
+	正则预定义
+		\s 空白 空格
+		\b 边界 如需用正则意义 则用 'r'
+		\d 数字 
+		\w world [0-9a-zA-Z]
+		
+		大写反义 \S 非空格 \D 非数字
+		'\w[0-9]'  \w [0-9]  只能匹配一个字母
+	量词
+		* >=0
+		+ >=1
+		? 0,1
+	手机号码正则:  /^1[345678]\d{9}$/
+'''
+number = '15696756582'
+number = re.match('^1[345678]\d{9}$', number)
+print(number)
+
+'''
+	{m} 固定m位
+	{m,} >= m位
+	{m,n} m~n 位
+'''
